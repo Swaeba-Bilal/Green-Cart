da8cc50756e30c080193f54e354155b1de300052
Swaeba-Bilal <swaibabilal02@gmail.com>
2026-08-25 10:33:11 +0500

    Added the seller apis

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -95,6 +95,7 @@ export const logout = async(req, res)=>{
 
     }
     catch(error){
+        console.log(error.message);
         res.json({success:false, message: error.message});
     }
 }
---
+++ server/routes/sellerRoute.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { sellerLogin, sellerLogout, isSellerAuth} from '../controllers/sellerController.js';
+import authSeller from '../middleware/authSeller.js';
+ const sellerRouter = express.Router();
+
+ sellerRouter.post('/login', sellerLogin);
+ sellerRouter.get('/is-auth', authSeller, isSellerAuth);
+ sellerRouter.get('/logout', sellerLogout);
+
+ export default sellerRouter;
