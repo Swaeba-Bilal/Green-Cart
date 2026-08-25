e15322cc09da1bd40688ccea15c0b2b8bae0a5e3
Swaeba-Bilal <swaibabilal02@gmail.com>
2026-08-25 11:19:27 +0500

    Added the product APis

--- server/server.js
+++ server/server.js
@@ -8,10 +8,14 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectClodinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000; 
  await connectDB ()
+
+ await connectClodinary()
 // Allow multiple origins
 
 const allowedOrigins = ['http://localhost:5173']
@@ -24,6 +28,7 @@ app.use(cors({origin: allowedOrigins, credentials:true}));
 app.get('/', (req, res)=> res.send("API is Working"));
 app.use('/api/user', userRouter); 
 app.use('/api/seller', sellerRouter); 
+app.use('/api/product', productRouter)
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
     
